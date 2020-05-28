a9456d1d2ce4f58071ae62b5688ffdf2e591a047
Nicholas Feitel <Nicholas.Feitel@gmail.com>
2020-05-28 10:40:49 -0400

    oauth p1

--- config/env.local.exs
+++ config/env.local.exs
@@ -1,5 +1,5 @@
 use Mix.Config
-System.put_env("GITHUB_CLIENT_ID", "...")
-System.put_env("GITHUB_CLIENT_SECRET", "...")
+System.put_env("GITHUB_CLIENT_ID", "ee64f091a2c96be5a306")
+System.put_env("GITHUB_CLIENT_SECRET", "b737868f4ff23a9b253539fddd4779a0a6c8058f")
 # config file needs to return config
 config :discuss, Discuss.Repo, %{}
